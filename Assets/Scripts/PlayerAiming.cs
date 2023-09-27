b4bf25f66cca9270592a96c9d5c2d43985ace10c
Valeriy <dobrotskiyvd@gmail.com>
2023-09-27 21:49:48 +0300

    Small fix

--- Assets/Scripts/PlayerAiming.cs
+++ Assets/Scripts/PlayerAiming.cs
@@ -3,10 +3,6 @@ using UnityEngine;
 
 public class PlayerAiming : MonoBehaviour
 {
-#if UNITY_EDITOR
-    [SerializeField] private Transform _debugTransform;
-#endif
-
     [SerializeField] private CameraViewToggle _viewToggle;
     [SerializeField] private LayerMask _aimColliderMask;
     [SerializeField] private Transform _hand;
@@ -28,9 +24,6 @@ public class PlayerAiming : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
         if (Physics.Raycast(ray, out RaycastHit rayHit, 999f, _aimColliderMask))
         {
-#if UNITY_EDITOR
-            _debugTransform.position = rayHit.point;
-#endif
             mouseAimWorldPos = rayHit.point;
         }
 
